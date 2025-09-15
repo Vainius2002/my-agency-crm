12b0005877f38ce6bba649e2a40af4c429e01599
Vainius Lunys <vainiusl@bpnlt.com>
2025-09-15 14:31:12 +0300

    Fixed some sqlalchemy errors

--- agency_crm/config.py
+++ agency_crm/config.py
@@ -7,7 +7,7 @@ load_dotenv(os.path.join(basedir, '.env'))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-please-change'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'instance', 'agency_crm.db')
+        'sqlite:///' + os.path.abspath(os.path.join(basedir, 'instance', 'agency_crm.db'))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join(basedir, os.environ.get('UPLOAD_FOLDER', 'app/static/uploads'))
     MAX_CONTENT_LENGTH = int(os.environ.get('MAX_CONTENT_LENGTH', 16 * 1024 * 1024))
--- agency_crm/run.py
+++ agency_crm/run.py
@@ -16,16 +16,4 @@ def make_shell_context():
             'ClientContact': ClientContact, 'MediaGroup': MediaGroup}
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-        
-        # Create default media groups if they don't exist
-        if MediaGroup.query.count() == 0:
-            default_groups = ['TV3 Group', 'M-1 Group', 'LRT', 'Radio Center', 'Clear Channel']
-            for group_name in default_groups:
-                mg = MediaGroup(name=group_name)
-                db.session.add(mg)
-            db.session.commit()
-            print("Default media groups created.")
-    
     app.run(debug=True, host='0.0.0.0', port=5001)
